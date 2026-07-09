6ad7058e181883a7a63e0d13a9b488cf1997cc6c
Chris Woods <301864178+chri5w00ds@users.noreply.github.com>
2026-07-09 23:00:51 +0200

    Data Collection

---
+++ Support Assistant.py
@@ -0,0 +1,22 @@
+print("Welcome to Chris's Support Assistant")
+
+print()
+
+name = input("What is your name? ")
+device = input("What device are you using? ")
+problem = input("What problem are you experiencing? ")
+urgency = input("Rate the urgency as Low, Medium or High! ")
+
+print()
+
+print("Support Summary")
+print("---------------")
+
+print(f"Customer: {name}")
+print(f"Device: {device}")
+print(f"Problem: {problem}")
+print(f"Urgency: {urgency}")
+
+print()
+
+print("Thank you, A support technician will review your issue soon.")
