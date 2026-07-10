281dec4e2a22ce830bb3a396f0a1268c1208617e
Chris Woods <301864178+chri5w00ds@users.noreply.github.com>
2026-07-10 08:23:28 +0200

    elif priority + all to lowercase

--- Support Assistant.py
+++ Support Assistant.py
@@ -5,7 +5,7 @@ print()
 name = input("What is your name? ")
 device = input("What device are you using? ")
 problem = input("What problem are you experiencing? ")
-urgency = input("Rate the urgency as Low, Medium or High! ")
+priority = input("Rate the urgency as Low, Medium or High! ")
 
 print()
 
@@ -15,10 +15,11 @@ print("---------------")
 print(f"Customer: {name}")
 print(f"Device: {device}")
 print(f"Problem: {problem}")
-print(f"Urgency: {urgency}")
 
-print()
+if priority == "High":
+    print("This issue should be prioritised.")
+elif priority == "Medium":
+    print("This issue should be handled soon.")
+else:
+    print("This issue can be scheduled for later.")
 
-print("Thank you, A support technician will review your issue soon.")
-
-print()
